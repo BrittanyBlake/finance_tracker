22637e6a8fc059bc6e7cc45566bffdedb0ddc333
Brittany Blake <brittany.blake49@gmail.com>
2020-07-02 14:10:58 +0200

    Generate stock model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_111540) do
+ActiveRecord::Schema.define(version: 2020_07_02_120735) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.decimal "last_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
