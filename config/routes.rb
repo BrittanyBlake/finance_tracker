119331fa9717183d2e8e64d251b46d16e6d8073e
Brittany Blake <brittany.blake49@gmail.com>
2020-07-03 16:01:18 +0200

    Display stock price in browser

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
 end
