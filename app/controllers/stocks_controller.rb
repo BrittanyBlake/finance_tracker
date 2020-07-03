a14211ed12c4a3f42e64d915aa1897ee13ddfaad
Brittany Blake <brittany.blake49@gmail.com>
2020-07-03 16:35:41 +0200

    handle invald search results

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,16 @@
 class StocksController < ApplicationController
     def search
-        @stock = Stock.new_lookup(params[:stock])
-        render 'users/my_portfolio'
+        if params[:stock].present?
+            @stock = Stock.new_lookup(params[:stock])
+            if @stock
+                render 'users/my_portfolio'
+            else
+                flash[:alert] = "Please enter a valid stock symbol"
+                redirect_to my_portfolio_path
+            end
+        else
+            flash[:alert] = "Please enter a valid stock symbol"
+            redirect_to my_portfolio_path
+        end
     end
 end
