da162be422f24fc3240a8790079f5a55b38f5fa3
Brittany Blake <brittany.blake49@gmail.com>
2020-07-05 13:17:30 +0200

    fixing errors

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,29 +1,16 @@
 class StocksController < ApplicationController
-    protect_from_forgery except: :search
-    def search
-<<<<<<< HEAD
-<<<<<<< HEAD
-     if params[:stock].present? 
-       @stock = Stock.new_lookup(params[:stock])
-       if @stock
-         respond_to do |format|
-           format.js { render partial: 'users/result' }
-         end
-       else
-         flash[:danger] = "You have entered an incorrect symbol"
-         redirect_to my_portfolio_path
-       end
-
+  def search
+     if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+        if @stock
+          render 'users/my_portfolio'
+        else
+          flash[:alert] = "Please enter a valid stock symbol"
+          redirect_to my_portfolio_path
+        end
      else
-       flash[:danger] = "You have entered an empty search string"
-       redirect_to my_portfolio_path
+      flash[:alert] = "Please enter a valid stock symbol"
+      redirect_to my_portfolio_path
      end
-   end
-=======
-=======
->>>>>>> parent of a14211e... handle invald search results
-        @stock = Stock.new_lookup(params[:stock])
-        render 'users/my_portfolio'
-    end
->>>>>>> parent of a14211e... handle invald search results
+  end
 end
